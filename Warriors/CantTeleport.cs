9519cf98000860dc96ca41d05d36e83f30a081ea
rudzzz <rudsolza13@gmail.com>
2023-04-21 14:03:13 -0300

    Teleport class

---
+++ Warriors/CantTeleport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarriorFight
+{
+    class CantTeleport : Teleports{
+        public string teleport() {
+            return "Fails at Teleporting";
+        }
+    }
+}
