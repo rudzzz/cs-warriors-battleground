a13ccff9e15960b304097ea61f388d70287da22f
rudzzz <rudsolza13@gmail.com>
2023-04-21 14:05:30 -0300

    Class where the Battle happens

---
+++ Warriors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WarriorFight{
+    class Program{
+        static void Main(string[] args){
+            Warrior sam = new Warrior("Sam", 100, 26, 10);
+            MagicWarrior carl = new MagicWarrior("Carl", 75, 20, 10, 50);
+
+            Battle.StartFight(sam, carl);
+        }
+    }
+}
