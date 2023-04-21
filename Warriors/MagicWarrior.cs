edb88f12c5ed00d0274cc44140fad60821c1a14b
rudzzz <rudsolza13@gmail.com>
2023-04-21 14:04:21 -0300

    Warrior classes

---
+++ Warriors/MagicWarrior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarriorFight
+{
+    class MagicWarrior : Warrior {
+        int teleportChance = 0;
+        CanTeleport teleportType = new CanTeleport();
+
+        public MagicWarrior(string name = "Magic Warrior",
+        double health = 0,
+        double attackMaximum = 0,
+        double blockMaximum = 0,
+        int teleportChance = 0) : base(name, health, attackMaximum, blockMaximum) { 
+            this.teleportChance = teleportChance;
+        }
+
+        public override double Block(){
+            Random randomNumber = new Random();
+            int randomDodge = randomNumber.Next(1, 100);
+
+            if(randomDodge < this.teleportChance){
+                Console.WriteLine($"{Name} {teleportType.teleport()}");
+                return 10000;
+            } else {
+                return base.Block();
+            }
+        }
+    }
+}
---
+++ Warriors/Warrior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarriorFight {
+    internal class Warrior {
+        public string Name { get; set; }
+        public double Health { get; set; }
+        public double AttackMaximum { get; set; }
+        public double BlockMaximum { get; set; }
+
+        Random randomNumber = new Random();
+
+        public Warrior(string name = "Warrior", double health = 0, double attackMaximum = 0, double blockMaximum = 0) { 
+            this.Name = name;
+            this.Health = health;
+            this.AttackMaximum = attackMaximum;
+            this.BlockMaximum = blockMaximum;
+        }
+
+        public double Attack(){
+            return randomNumber.Next(1, (int)AttackMaximum);
+        }
+        public virtual double Block(){
+            return randomNumber.Next(1, (int)BlockMaximum);
+        }
+    }
+}
